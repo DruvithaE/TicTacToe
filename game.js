4dee5e37a851ee35f17b76f87d4bb4f1fd08df5e
DruvithaE <141492198+DruvithaE@users.noreply.github.com>
2024-07-14 13:25:57 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,79 @@
+// The Player class
+class Player {
+    constructor(socketID, name, symbol) {
+        this.socketID = socketID;
+        this.name = name;
+        this.symbol = symbol;
+    }
+}
+
+// The Game class
+class Game {
+    constructor(playerOne, playerTwo, roomID, turn) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+        this.roomID = roomID;
+        this.turn = turn;
+        this.board = [[null, null, null], [null, null, null], [null, null, null]];
+    }
+
+    // Clears the game board
+    clearBoard() {
+        this.board = [[null, null, null], [null, null, null], [null, null, null]];
+    }
+
+    // Resets the game
+    resetGame() {
+        this.clearBoard();
+        this.turn = 1;
+    }
+
+    // Marks the cell with the given ID with the given symbol
+    markBoard(symbol, cellID) {
+        cellID--;
+        let row = Math.floor(cellID / 3);
+        let col = cellID % 3;
+        this.board[row][col] = symbol;
+    }
+
+    // Checks if the given symbol has won the game
+    checkWin(symbol, cellID) {
+        cellID--;
+        let row = Math.floor(cellID / 3);
+        let col = cellID % 3;
+
+        // Check row
+        if (this.board[row][0] == symbol && this.board[row][1] == symbol && this.board[row][2] == symbol)
+            return true;
+
+        // Check column
+        if (this.board[0][col] == symbol && this.board[1][col] == symbol && this.board[2][col] == symbol)
+            return true;
+
+        // Check diagonals
+        if (this.board[0][0] == symbol && this.board[1][1] == symbol && this.board[2][2] == symbol)
+            return true;
+        if (this.board[0][2] == symbol && this.board[1][1] == symbol && this.board[2][0] == symbol)
+            return true;
+
+        return false;
+    }
+
+    // Checks if the game is a draw
+    checkDraw() {
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (this.board[i][j] == null) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
+
+module.exports = {
+    Player: Player,
+    Game: Game
+};
